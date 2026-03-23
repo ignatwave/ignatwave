817ec56adb8f06d5a491c44397b6201aef8fe326
Mohammed Fayiz k <mhdfayizk@gmail.com>
2026-03-23 14:38:32 +0530

    first

---
+++ app/page.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import TechMarquee from './components/TechMarquee'
+import Services from './components/Services'
+import WhyUs from './components/WhyUs'
+import Work from './components/Work'
+import TechStack from './components/TechSack'
+import Team from './components/Team'
+import Testimonials from './components/Testimonials'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <TechMarquee />
+      <Services />
+      <WhyUs />
+      {/* <Work /> */}
+      <TechStack />
+     {/* <Team /> */}
+      {/* <Testimonials /> */}
+      <Contact />
+      <Footer />
+    </main>
+  )
+}
